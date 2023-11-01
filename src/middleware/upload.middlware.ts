b0a7bb9d024b1c5471f8ac3391dc9f21d4e9e6ce
IhorKozloff <87847488+IhorKozloff@users.noreply.github.com>
2023-11-01 19:09:57 +0200

    Update upload.middlware.ts

--- src/middleware/upload.middlware.ts
+++ src/middleware/upload.middlware.ts
@@ -8,16 +8,16 @@ const fs = require('fs');
 const storage = multer.diskStorage({
     destination ( req, file, cb ) {
 
-        const uploadDirectory = path.join(__dirname, 'users-images');
+        // const uploadDirectory = path.join(__dirname, 'users-images');
 
-        if (!fs.existsSync(uploadDirectory)) {
-            fs.mkdirSync(uploadDirectory);
-        }
+        // if (!fs.existsSync(uploadDirectory)) {
+        //     fs.mkdirSync(uploadDirectory);
+        // }
 
         console.log(file)
 
-        console.log(uploadDirectory)
-        cb(null, uploadDirectory);
+        //console.log(uploadDirectory)
+        cb(null, 'src/public/users-images');
     },
     filename (req, file, cb) {
         cb(null, `${Date.now()}-${file.originalname}`);
